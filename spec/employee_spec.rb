2dd9ad10e70b5e0646a25a9a18f7345a4098d5d3
Ezra Sandzer-Bell and Luke Philips <ezra@>
2016-12-12 11:14:58 -0800

    pass unit test for Employee to division

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,9 +1,16 @@
 require('spec_helper')
 
 describe Employee do
+  before do
+    @billy = Employee.create(:name => 'Billy')
+  end
   it 'creates an employee' do
-    billy = Employee.create(:name => 'Billy')
-    expect(Employee.all).to eq([billy])
+    expect(Employee.all).to eq([@billy])
     expect(Employee.all[0].name).to eq('Billy')
+  end
+  it 'tells which division an employee belongs to' do
+    hr = Division.create(:name => 'HR')
+    danny = Employee.create(:name => 'Danny', :division_id => hr.id)
+    expect(hr.employees).to eq([danny])
   end
 end
